579c3379b42ee74f0d237870c7b4a553d02b00f8
Xiaoyu <xiaoyuliu.lxy@alibaba-inc.com>
2015-11-10 16:14:38 +0800

    add tmux super menu

---
+++ tmux_attach.sh
@@ -0,0 +1,36 @@
+# abort if we're already inside a TMUX session
+[ "$TMUX" == "" ] || exit 0 
+
+if [ "$SSH_TTY" == "" ]; then
+  exit 0
+fi
+
+# startup a "default" session if none currently exists
+#tmux has-session -t _default || tmux new-session -s _default -d
+
+# present menu for user to choose which workspace to open
+PS3="Please choose your session: "
+options=($(tmux list-sessions -F "#S") "NEW SESSION" "BASH" "GDB")
+echo "Available sessions"
+echo "------------------"
+echo " "
+select opt in "${options[@]}"
+do
+  case $opt in
+    "NEW SESSION")
+      read -p "Enter new session name: " SESSION_NAME
+      tmux new -s "$SESSION_NAME"
+      break
+      ;;
+    "BASH")
+      bash --login
+      break;;
+    "GDB")
+      screen -r 
+      break;;
+    *) 
+      tmux attach-session -t $opt 
+      break
+      ;; 
+  esac
+done    
